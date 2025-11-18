e3519dd00593bc8a7d345fee23a4a93083747ce3
jheinon2021 <joonas.heinonen@wunder.io>
2025-11-18 11:07:26 +0200

    #17 Refactored the phpstan tooling to be ran via _run-scripts.sh

--- commands/web/wunderio-core-phpstan.sh
+++ commands/web/wunderio-core-phpstan.sh
@@ -2,24 +2,10 @@
 
 #ddev-generated
 
-## Description: Run PHPStan code scanning tool.
-## Usage: phpstan
-## Example: "ddev phpstan"
+## Description: Runs PHPUnit commands.
+## Usage: phpunit
+## Example: "ddev phpunit"
 ## ExecRaw: true
 ## ProjectTypes: drupal9,drupal10,drupal11
 
-# Check that PHPStan is installed.
-if [ ! -f "$DDEV_COMPOSER_ROOT/vendor/bin/phpstan" ]; then
-  echo "Composer binaries for PHPStan missing; exiting early."
-  echo "You can install it with 'ddev composer require phpstan/phpstan --dev'."
-  exit 0
-fi
-
-# Check that phpstan.neon or phpstan.neon.dist exists.
-if [ ! -f "$DDEV_COMPOSER_ROOT/phpstan.neon" ] && [ ! -f "$DDEV_COMPOSER_ROOT/phpstan.neon.dist" ]; then
-  echo "phpstan.neon or phpstan.neon.dist not found! Please generate one to configure PHPStan."
-  exit 0
-fi
-
-set -eu
-$DDEV_COMPOSER_ROOT/vendor/bin/phpstan "$@"
+"$WUNDERIO_GLOBAL_CACHE_WUNDERIO/core/_run-scripts.sh" tooling-phpstan.sh "$@"
