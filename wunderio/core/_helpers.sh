b2e22ae6e9464d39e89a315cba5c6066eb11d585
DDEV User <nobody@example.com>
2025-12-18 08:17:46 +0200

    #30: update comments for helper functions

--- wunderio/core/_helpers.sh
+++ wunderio/core/_helpers.sh
@@ -10,7 +10,7 @@ if [[ -n "${WUNDERIO_DEBUG:-}" ]]; then
     set -x
 fi
 
-# Function to display status message
+# Function to display status message.
 display_status_message() {
     local color_green="\033[38;5;70m"
     local color_reset="\033[0m"
@@ -28,6 +28,7 @@ display_error_message() {
     printf "${color_red}${message}${color_reset}\n"
 }
 
+# Function to display warning message.
 display_warning_message() {
     local color_yellow="\033[0;33m"
     local color_reset="\033[0m"
