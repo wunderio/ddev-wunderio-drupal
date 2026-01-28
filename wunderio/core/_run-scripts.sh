0beeb05efae1259f194b5de7c4be4598626e0d8c
Santeri Kinnunen <santeri.kinnunen@wunder.io>
2026-01-28 13:23:25 +0200

    Update script runner to for new folder structure, create volume for hooks, adjust initial hooks for the script runner

--- wunderio/core/_run-scripts.sh
+++ wunderio/core/_run-scripts.sh
@@ -11,43 +11,66 @@ if [[ -n "${WUNDERIO_DEBUG:-}" ]]; then
     set -x
 fi
 
-# This script when ran in host, does not have the WUNDERIO_GLOBAL_CACHE_ROOT and
-# WUNDERIO_GLOBAL_CACHE_WUNDERIO environment variables set. So we need to set
-# them here.
-GLOBAL_CACHE_ROOT="${WUNDERIO_GLOBAL_CACHE_ROOT:-/mnt/ddev-global-cache}"
-GLOBAL_CACHE_WUNDERIO="${WUNDERIO_GLOBAL_CACHE_WUNDERIO:-$GLOBAL_CACHE_ROOT/wunderio}"
+# The path to the initial script (used to resolve the actual directory)
+# Resolve the script path to handle symbolic links
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+    DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
 
+# Variable to hold the resolved script path
+export WUNDERIO_GLOBAL_SCRIPT_ROOT="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+
+# Use the function to get the directory of the script
+# Make WUNDERIO_GLOBAL_SCRIPT_ROOT available.
 # Expose helpers to tooling.
-source "$(dirname "${BASH_SOURCE[0]}")/_helpers.sh"
+source "$WUNDERIO_GLOBAL_SCRIPT_ROOT/_helpers.sh"
 
-script_name="$1"
-# Remove the first argument (the script name) to get the remaining arguments
+# Remove the first argument (the method)
+script_or_hook="$1"
 shift 1
 
-custom_script="$DDEV_APPROOT/.ddev/wunderio/custom/$script_name"
+# Check if the string is neither tooling nor hooks.
+if [[ "$script_or_hook" != "tooling" && "$script_or_hook" != "hooks" ]]; then
+    display_error_message "wdr-core [tooling|hooks] script-name"
+    exit 1
+fi
+
+# Remove the second argument (the script)
+script_name="$1"
+shift 1
+
+# Legacy support for older scripts.
+app_root_customs="$DDEV_APPROOT/.ddev/wunderio/custom"
+legacy_custom_script="$app_root_customs/$script_or_hook-$script_name"
+custom_script="$app_root_customs/$script_or_hook/$script_name"
 
 # Sometimes this script is run on the host, sometimes inside the DDEV container
 # so we need to check the directory to determine where the script is
 # running from. Mostly it's in the container, but at least one time in
 # config.wunderio.yaml we call it on the host via exec-host.
-if [ -d "$GLOBAL_CACHE_ROOT" ]; then
-  core_script="$GLOBAL_CACHE_WUNDERIO/core/$script_name"
-else
-  core_script="$HOME/.ddev/wunderio/core/$script_name"
-fi
+core_script="$WUNDERIO_GLOBAL_SCRIPT_ROOT/$script_or_hook/$script_name"
 
+echo "$legacy_custom_script";
 # Check if the custom script exists and is executable
-if [ -x "$custom_script" ]; then
+if [ -x "$legacy_custom_script" ]; then
+    display_status_message "Running custom script: $legacy_custom_script"
+    display_warning_message "This script runs on legacy hooks"
+    display_warning_message "  Consider to moving it into .ddev/wunderio/custom/$script_or_hook/$script_name"
+    # Run the script and pass all remaining arguments.
+    "$legacy_custom_script" "$@"
+elif [ -x "$custom_script" ]; then
     display_status_message "Running custom script: $custom_script"
     # Run the script and pass all remaining arguments.
     "$custom_script" "$@"
 elif [ -x "$core_script" ]; then
     # If the custom script doesn't exist, run the core script.
     display_status_message "Running core script: $core_script"
-
     # Run the script and pass all remaining arguments.
     "$core_script" "$@"
 else
-    echo "Script not found or not executable: $script_name"
+    display_error_message "Script not found or not executable: $script_name"
     exit 1
 fi
